fde72c0a8ce9074f8417aec6503dc8ba131976b2
Adrián Cisneros <nicolocisneros@gmail.com>
2025-01-05 00:56:55 -0500

    fix: Variable birthdate añadido al DTO de listado de info del paciente

--- src/main/java/com/plenamente/sgt/domain/dto/PatientDto/ListPatient.java
+++ src/main/java/com/plenamente/sgt/domain/dto/PatientDto/ListPatient.java
@@ -2,6 +2,7 @@ package com.plenamente.sgt.domain.dto.PatientDto;
 
 import com.plenamente.sgt.domain.dto.TutorDto.TutorDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record ListPatient(
@@ -10,6 +11,7 @@ public record ListPatient(
         String paternalSurname,
         String maternalSurname,
         String dni,
+        LocalDate birthdate,
         int age,
         Long planId,
         List<TutorDTO> tutors,
