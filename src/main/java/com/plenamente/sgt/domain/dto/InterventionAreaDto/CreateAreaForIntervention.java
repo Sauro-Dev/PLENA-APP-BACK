27fbff8a74e1fc41e1c9afb4bead6b4ad58078ac
FluidityCore <Bertlas.u@gmail.com>
2025-01-08 22:16:08 -0500

    Cambio a opcional Descripcion de areas

--- src/main/java/com/plenamente/sgt/domain/dto/InterventionAreaDto/CreateAreaForIntervention.java
+++ src/main/java/com/plenamente/sgt/domain/dto/InterventionAreaDto/CreateAreaForIntervention.java
@@ -1,9 +1,10 @@
 package com.plenamente.sgt.domain.dto.InterventionAreaDto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Null;
 
 public record CreateAreaForIntervention(
         @NotBlank String name,
-        @NotBlank String description
-) {
+        @Null String description
+        ) {
 }
