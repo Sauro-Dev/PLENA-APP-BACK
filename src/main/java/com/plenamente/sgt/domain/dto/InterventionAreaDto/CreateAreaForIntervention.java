1d904dd940ea866b5f0ea0a1b06be216e55fa0fb
jhoneiro <jhoneiro12@hotmail.com>
2025-01-09 10:44:32 -0500

    el campo de descripcion AHORA SI ACEPTA VACIOS y no se cae nada

--- src/main/java/com/plenamente/sgt/domain/dto/InterventionAreaDto/CreateAreaForIntervention.java
+++ src/main/java/com/plenamente/sgt/domain/dto/InterventionAreaDto/CreateAreaForIntervention.java
@@ -1,10 +1,11 @@
 package com.plenamente.sgt.domain.dto.InterventionAreaDto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 
 public record CreateAreaForIntervention(
         @NotBlank String name,
-        @Null String description
+        @NotNull String description
         ) {
 }
